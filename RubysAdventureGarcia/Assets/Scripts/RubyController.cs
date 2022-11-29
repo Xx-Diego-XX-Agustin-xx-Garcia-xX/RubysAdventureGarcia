39e571da97603ed0b66c99ad39c8ed96f43419ed
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2022-11-29 11:44:53 -0800

    RUBYSADVENTURE
    e

--- RubysAdventureGarcia/Assets/Scripts/RubyController.cs
+++ RubysAdventureGarcia/Assets/Scripts/RubyController.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    public float speed = 2.5f;
+    public int maxHealth = 1;
+    int currentHealth;
+    Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 2.5f * horizontal * Time.deltaTime;
-        position.y = position.y + 2.5f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
